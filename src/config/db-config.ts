88c51929c34ab9e998b357745885d965b02da550
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2026-04-17 11:38:24 +0545

    fix: supabase configs

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -23,12 +23,6 @@ export const typeOrmConfigs = () => {
     ssl: {
       rejectUnauthorized: false,
     },
-
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
   };
 
   return obj;
