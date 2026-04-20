57706a89c009373ace1f85741c910049a099de40
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2026-04-20 12:03:34 +0545

    fix: throttler

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { SkipThrottle } from '@nestjs/throttler';
 import { DataSource } from 'typeorm';
 
+@SkipThrottle()
 @Controller('health-check')
 export class HealthController {
   constructor(private dataSource: DataSource) {}
